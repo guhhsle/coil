317eedfb67d180784c3e10f27b3c74dda554d1e4
Guhhsle <guhhsle@proton.me>
2024-04-30 19:14:27 +0200

    Cleaner prefs

--- lib/template/prefs.dart
+++ lib/template/prefs.dart
@@ -24,24 +24,22 @@ Future<void> nextPref(String pref, List<String> list, {bool refresh = false}) as
   await setPref(pref, list[(list.indexOf(pf[pref]) + 1) % list.length], refresh: refresh);
 }
 
-dynamic getPref(pString) {
-  if (prefs.get(pString) is List) {
-    return prefs.getStringList(pString);
-  } else {
-    return prefs.get(pString);
-  }
+dynamic getPref(name) {
+  var val = prefs.get(name);
+  if (val is List) return prefs.getStringList(name);
+  return val;
 }
 
-Future setPref(String pString, var value, {bool refresh = false}) async {
-  pf[pString] = value;
+Future setPref(String name, var value, {bool refresh = false}) async {
+  pf[name] = value;
   if (value is int) {
-    await prefs.setInt(pString, value);
+    await prefs.setInt(name, value);
   } else if (value is bool) {
-    await prefs.setBool(pString, value);
+    await prefs.setBool(name, value);
   } else if (value is String) {
-    await prefs.setString(pString, value);
+    await prefs.setString(name, value);
   } else if (value is List<String>) {
-    await prefs.setStringList(pString, pf[pString]);
+    await prefs.setStringList(name, pf[name]);
   }
   if (refresh) refreshInterface();
   refreshLayer();
